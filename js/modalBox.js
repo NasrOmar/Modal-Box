10f5217e8c9042d6066e5b57670b779f4c192bfa
Omar Nasr <nasr.omar@icloud.com>
2019-10-23 18:40:43 +0200

    added js

--- js/modalBox.js
+++ js/modalBox.js
@@ -1,1 +1,23 @@
 // JavaScript Document
+
+var btn = document.getElementById("modal-button");
+
+var modal = document.getElementById("modal");
+
+var modalBackground = document.getElementById("modal-background");
+
+var modalContent = document.getElementById("modal-content");
+
+var btnClose = document.getElementById("modal-close-btn");
+
+btn.addEventListener ("click", btnClick => {
+	modal.classList.add("modal-visible");
+	modalBackground.classList.add("modal-background-visibile");
+	modalContent.classList.add("modal-content-visible");
+});
+
+btnClose.addEventListener ("click", btnClickClose => {
+	modal.classList.remove("modal-visible");
+	modalBackground.classList.remove("modal-background-visibile");
+	modalContent.classList.remove("modal-content-visible");
+});
